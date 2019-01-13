0aacfe0250c5504ef058f941556c1047515248fc
Ir0nDru <tiller-nin@yandex.ru>
2019-01-13 17:43:11 +0300

    Created camera execution project

---
+++ Recorder.Executor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Recorder.Executor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessStartInfo info = new ProcessStartInfo
+            {
+                FileName = "ping",
+                Arguments = $"localhost",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+                //FileName = "cmd.exe",
+                //Arguments = "dir",
+                //RedirectStandardOutput = true,
+                //CreateNoWindow = true,
+                //UseShellExecute = false
+            };
+            Process proc = new Process
+            {
+                StartInfo = info
+            };
+            proc.Start();
+            string str = proc.StandardOutput.ReadToEnd();
+            proc.WaitForExit();
+            Console.WriteLine(str);
+            Console.ReadLine();
+        }        
+    }
+}
