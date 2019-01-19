5df64b53d9ebcb38466c0911059c25d30dc2c7ac
Razzzdolbay <avasilyev1997@gmail.com>
2019-01-19 23:21:06 +0300

    add executor add job server

---
+++ Recorder.Executor/ExecutorHelper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RtspClientSharp;
+using RtspClientSharp.Rtsp;
+
+namespace Recorder.Executor
+{
+    public static class ExecutorHelper
+    {
+        public static void MakeRecordTask(string ipAdress, string timeSpan)
+        {
+            var serverUri = new Uri($"rtsp://{ipAdress}");
+            var credentials = new NetworkCredential("admin", "Supervisor");
+            var connectionParameters = new ConnectionParameters(serverUri, credentials);
+            var cancellationTokenSource = new CancellationTokenSource();
+            RecordAsync(connectionParameters, cancellationTokenSource.Token, ipAdress, timeSpan).Wait(CancellationToken.None);
+        }
+
+        private static async Task RecordAsync(ConnectionParameters connectionParameters, CancellationToken token, string ipAdress, string timeSpan)
+        {
+            try
+            {
+                TimeSpan delay = TimeSpan.FromSeconds(5);
+
+                using (var rtspClient = new RtspClient(connectionParameters))
+                {
+                    Console.WriteLine("Connecting...");
+
+                    try
+                    {
+                        await rtspClient.ConnectAsync(token);
+                        Console.WriteLine("Connected");
+                    }
+                    catch (OperationCanceledException)
+                    {
+                    }
+                    catch (RtspClientException e)
+                    {
+                        Console.WriteLine(e.ToString());
+                        await Task.Delay(delay, token);
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+            }
+            string args = $"-i rtsp://@{ipAdress} -t {timeSpan} -acodec copy -vcodec copy {DateTime.Now.ToFileTime().ToString()}.mp4";
+            var process = new Process
+            {
+                StartInfo =
+                {
+                    FileName = "ffmpeg.exe",
+                    Arguments = args,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    RedirectStandardInput = true,
+                }
+            };
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+}
--- Recorder.Executor/Program.cs
+++ Recorder.Executor/Program.cs
@@ -1,34 +1,50 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using Hangfire;
+using Recorder.Service;
+using Recorder.Service.Services;
+using System.Net.Http;
+using Recorder.Service.Entities;
+using RestSharp;
 
 namespace Recorder.Executor
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            ProcessStartInfo info = new ProcessStartInfo
+           
+            using (new BackgroundJobServer())
             {
-                FileName = "ping",
-                Arguments = $"localhost",
-                RedirectStandardOutput = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-                //FileName = "cmd.exe",
-                //Arguments = "dir",
-                //RedirectStandardOutput = true,
-                //CreateNoWindow = true,
-                //UseShellExecute = false
-            };
-            Process proc = new Process
-            {
-                StartInfo = info
-            };
-            proc.Start();
-            string str = proc.StandardOutput.ReadToEnd();
-            proc.WaitForExit();
-            Console.WriteLine(str);
-            Console.ReadLine();
-        }        
+                var clientCameraService = new RestClient(new Uri("http://localhost:5000"));
+                while (true)
+                {
+                    Thread.Sleep(15000);
+                    var request = new RestRequest("cameras/actual", Method.GET);
+                    Camera[] cameras = clientCameraService.ExecuteGetTaskAsync<Camera[]>(request)
+                        .GetAwaiter()
+                        .GetResult()
+                        .Data;
+                    foreach (Camera camera in cameras)
+                    {
+                        foreach (Record record in camera.Records)
+                        {
+                            var timeToRecord = Convert.ToInt32(DateTime.Now.Subtract(record.StartTime).TotalSeconds);
+                            var recordingTimeSpan = Convert.ToInt32((record.EndTime - record.EndTime).TotalSeconds);
+
+                            if (timeToRecord < 300.0)
+                            {
+                                BackgroundJob.Schedule(
+                                    () => ExecutorHelper.MakeRecordTask(camera.IpAddress, recordingTimeSpan.ToString()),
+                                    TimeSpan.FromSeconds(timeToRecord));
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
